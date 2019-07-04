650747b45f979fba03e969feb4f5a002c2068ece
Kartikey Kumar Srivastava <kartikeyshrivastava@gmail.com>
2019-07-04 21:30:42 +0530

    Completed second feature

--- app/src/main/java/croom/konekom/in/a100pitask/constant/Constants.java
+++ app/src/main/java/croom/konekom/in/a100pitask/constant/Constants.java
@@ -2,5 +2,8 @@ package croom.konekom.in.a100pitask.constant;
 
 public class Constants {
     public static final String BASE_URL = "https://bittrex.com/api/v1.1/public/";
+    public static final String CURRENT_POS = "current_pos";
+    public static final String MyPREFERENCES = "MyPrefs" ;
+
 
 }
---
+++ app/src/main/java/croom/konekom/in/a100pitask/widget/CurrencyWidgetProvider.java
@@ -0,0 +1,120 @@
+package croom.konekom.in.a100pitask.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.List;
+
+import croom.konekom.in.a100pitask.R;
+import croom.konekom.in.a100pitask.constant.Constants;
+import croom.konekom.in.a100pitask.database.AppDatabase;
+import croom.konekom.in.a100pitask.model.Currency;
+
+public class CurrencyWidgetProvider extends AppWidgetProvider {
+    int current_pos = 0;
+    private List<Currency> currencyArrayList;
+    private AppDatabase appDatabase;
+
+
+    private static final String NEXT_CURRENCY = "nextcurrency";
+    private static final String PREVIOUS_CURRENCY = "previouscurrency";
+    private SharedPreferences sharedpreferences;
+    private SharedPreferences.Editor editor;
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        appDatabase = AppDatabase.getInstance(context);
+        sharedpreferences = context.getSharedPreferences(Constants.MyPREFERENCES, Context.MODE_PRIVATE);
+        if(!sharedpreferences.contains(Constants.CURRENT_POS)){
+            sharedpreferences.edit().putInt(Constants.CURRENT_POS,0).apply();
+        }
+            current_pos = sharedpreferences.getInt(Constants.CURRENT_POS, 0);
+
+
+        for (int appwidgetId : appWidgetIds) {
+
+            currencyArrayList = appDatabase.userDao().getAllCurrencies();
+            final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+
+            if(currencyArrayList.size()>0) {
+                views.setTextViewText(R.id.currency, currencyArrayList.get(current_pos).getCurrency());
+                views.setTextViewText(R.id.currencyLong, currencyArrayList.get(current_pos).getCurrencyLong());
+                views.setTextViewText(R.id.txFee, currencyArrayList.get(current_pos).getTxFee() + "");
+                views.setOnClickPendingIntent(R.id.previousCurrency,
+                        getPendingSelfIntent(context, PREVIOUS_CURRENCY));
+                views.setOnClickPendingIntent(R.id.nextCurrency,
+                        getPendingSelfIntent(context, NEXT_CURRENCY));
+            }
+            appWidgetManager.updateAppWidget(appwidgetId, views);
+        }
+    }
+    private PendingIntent getPendingSelfIntent(Context context, String action) {
+        // An explicit intent directed at the current class (the "self").
+        Intent intent = new Intent(context, getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, 0);
+    }
+    private void onUpdate(Context context) {
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance
+                (context);
+
+        // Uses getClass().getName() rather than MyWidget.class.getName() for
+        // portability into any App Widget Provider Class
+        ComponentName thisAppWidgetComponentName =
+                new ComponentName(context.getPackageName(),getClass().getName()
+                );
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                thisAppWidgetComponentName);
+        onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        sharedpreferences = context.getSharedPreferences(Constants.MyPREFERENCES, Context.MODE_PRIVATE);
+        appDatabase = AppDatabase.getInstance(context);
+
+        currencyArrayList = appDatabase.userDao().getAllCurrencies();
+
+        if (PREVIOUS_CURRENCY.equals(intent.getAction())) {
+            current_pos = sharedpreferences.getInt(Constants.CURRENT_POS, 0);
+            if (current_pos != 0) {
+
+
+                current_pos = current_pos - 1;
+                editor = sharedpreferences.edit();
+                editor.putInt(Constants.CURRENT_POS, current_pos);
+                editor.apply();
+
+            }
+
+
+            onUpdate(context);
+        }
+
+        else if (NEXT_CURRENCY.equals(intent.getAction())) {
+            current_pos = sharedpreferences.getInt(Constants.CURRENT_POS, 0);
+
+                if (current_pos != currencyArrayList.size() - 1) {
+
+                current_pos = current_pos + 1;
+                editor = sharedpreferences.edit();
+                editor.putInt(Constants.CURRENT_POS, current_pos);
+                editor.apply();
+
+            }
+
+            onUpdate(context);
+        }
+    }
+
+
+
+}
